9ccf35b77eaa08d7edd2731e3ce8a6f47d22ce8f
Jona JOACHIM <jona@joachim.cc>
2026-05-11 15:51:37 +0200

    fix: __all__ contains strings

--- src/pmxmc/io/__init__.py
+++ src/pmxmc/io/__init__.py
@@ -3,4 +3,4 @@ from .read_nonmem_dataset_padded import (
     read_nonmem_dataset as read_nonmem_dataset_padded,
 )
 
-__all__ = [read_nonmem_dataset, read_nonmem_dataset_padded]
+__all__ = ["read_nonmem_dataset", "read_nonmem_dataset_padded"]
